dca66985b1e0a64b77ab16ddb2e13e0733c95206
BDunnCode <BDunnCode@github.com>
2024-01-04 00:25:49 -0600

    mostly nonsensical tinkering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 
 function App() {
+  const title ='Welcome to the spot like, broh'
+  const likes = 350
+  // const person = { name: 'yoshi', age: 30 }
+  const link = "https://www.google.com"
+
   return (
     <div className="App">
       <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        {/* <p>{ person }</p> */}
 
+        <p>{ 10 }</p>
+        <p>{ "suh" }</p>
+        <p>{ [1,2,3, ' and to the ', 4] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={ link }>GoogCity</a>
       </div>
     </div>
   );
