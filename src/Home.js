cf956257f7e4bee4def3794cd4aa9bfe532db738
BDunnCode <BDunnCode@github.com>
2024-01-04 17:41:04 -0600

    Homepage stripped empty again

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,8 @@
+import { useState } from 'react'
+
 const Home = () => {
-
-  const handleClick = (e) => {
-    console.log('greetings, fellow doggos', e)
-  }
-
-  const handleClickAgain = (name) => {
-    console.log('Hello, ' + name)
-  }
-
   return ( 
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={() => handleClickAgain('Gucci Doug')}>Click me again</button>
     </div>
    );
 }
