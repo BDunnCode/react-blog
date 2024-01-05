8781e31d326f5885a1c4c62080fbc6732ae90b1f
BDunnCode <BDunnCode@github.com>
2024-01-04 21:08:10 -0600

    removed all blog deletion functionality

--- src/Home.js
+++ src/Home.js
@@ -2,31 +2,23 @@ import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'Speedy I', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Marley', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Speedy II', id: 3 },
-  ])
+  const [blogs, setBlogs] = useState(null)
 
-  const [name, setName] = useState('Speedy II')
-
-  const handleDelete = (id) => { 
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs)
-  }
 
 // Runs on every render. At the beginning and every time state data is changed.
   useEffect(() => {
-    console.log('use effect ran')
+    fetch('http://localhost:8000/blogs')
+      .then(res =>  {
+        return res.json()
+      }).then((data) => {
+        console.log(data)
+        setBlogs(data)
+      })
   }, [])
 
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title="All the Bloggos from the Doggos!" handleDelete={handleDelete} />
-      <button onClick={
-        () => setName('Speedy I')}
-        >Change Name</button>
-      <p>{ name }</p>
+      {blogs && <BlogList blogs={blogs} title="All the Bloggos from the Doggos!" /> }
     </div>
    );
 }
