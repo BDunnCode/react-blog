bf3f6dccbece146c738903decfd267a519c930bd
BDunnCode <BDunnCode@github.com>
2024-01-04 21:43:56 -0600

    fetch and error handling

--- src/Home.js
+++ src/Home.js
@@ -3,21 +3,36 @@ import BlogList from './BlogList'
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
+  const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null)
 
 
 // Runs on every render. At the beginning and every time state data is changed.
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res =>  {
-        return res.json()
-      }).then((data) => {
-        console.log(data)
-        setBlogs(data)
-      })
-  }, [])
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+           if(!res.ok) {
+            throw Error('could not fetch the data for that resource')
+           }
+          return res.json()
+        })
+        .then(data => {
+          setBlogs(data)
+          setIsPending(false)
+          setError(null)
+        })
+        .catch(err => {
+          setIsPending(false)
+          setError(err.message)
+        })
+  }, 1000)
+}, [])
 
   return ( 
     <div className="home">
+      {error && <div>{ error }</div> }
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All the Bloggos from the Doggos!" /> }
     </div>
    );
